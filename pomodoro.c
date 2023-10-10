d11d27ba982a9a5d0a20ead51dad03e7a9a9a3ab
Ayush Rawat <ayush.rawat3@gmail.com>
2023-10-10 12:11:53 -0700

    Added default path for config file

--- pomodoro.c
+++ pomodoro.c
@@ -5,6 +5,7 @@
 #include <termios.h>
 #include <sys/ioctl.h>
 #include <stdbool.h>
+#include <sys/stat.h>
 
 typedef struct {
     int workRed, workGreen, workBlue;
@@ -12,7 +13,7 @@ typedef struct {
     int workMinutes, breakMinutes, sessions;
 } Config;
 
-const char *CONFIG_FILE_PATH = "pomodoro_config.txt";
+char CONFIG_FILE_PATH[256] = "pomodoro_config.txt";
 Config config;
 
 void loadConfig(Config *config);
@@ -22,22 +23,65 @@ void displayTimer(Config *config);
 void displayProgress(int totalSeconds, int elapsedSeconds, char* label, int session, Config *config);
 void sendNotification(char* message);
 void enableRawMode();
+void expandUserHomeDirectory();
+void handleSignal(int sig);
+void disableRawMode();
+
+void handleSignal(int sig) {
+    disableRawMode();
+    exit(sig);
+}
+
+void expandUserHomeDirectory() {
+    char *home = getenv("HOME");
+    if (home != NULL) {
+        snprintf(CONFIG_FILE_PATH, sizeof(CONFIG_FILE_PATH), "%s/.config/pomodoro/pomodoro_config.txt", home);
+    } 
+}
 
 void loadConfig(Config *config) {
+    setDefaultConfig(config);
+    expandUserHomeDirectory();
+
     FILE *file = fopen(CONFIG_FILE_PATH, "r");
     if (file) {
-        if (9 != fscanf(file, "%d %d %d %d %d %d %d %d %d",
-                        &config->workRed, &config->workGreen, &config->workBlue, 
-                        &config->breakRed, &config->breakGreen, &config->breakBlue,
-                        &config->workMinutes, &config->breakMinutes, &config->sessions)) {
-            setDefaultConfig(config);
-            saveConfig(config, false);
+        char line[256];
+        while (fgets(line, sizeof(line), file)) {
+            char key[256];
+            int value1, value2, value3;
+
+            if (sscanf(line, "%[^=] = (%d,%d,%d)", key, &value1, &value2, &value3) == 4) {
+                if (value1 >= 0 && value1 <= 255 && value2 >= 0 && value2 <= 255 && value3 >= 0 && value3 <= 255) {
+                    if (strcmp(key, "workcolor") == 0) {
+                        config->workRed = value1;
+                        config->workGreen = value2;
+                        config->workBlue = value3;
+                    } else if (strcmp(key, "breakcolor") == 0) {
+                        config->breakRed = value1;
+                        config->breakGreen = value2;
+                        config->breakBlue = value3;
+                    }
+                } else {
+                    printf("Error: Invalid color values in config file. Using default values.\n");
+                }
+            } else if (sscanf(line, "%[^=] = %d", key, &value1) == 2) {
+                if (value1 > 0) {
+                    if (strcmp(key, "work") == 0) {
+                        config->workMinutes = value1;
+                    } else if (strcmp(key, "break") == 0) {
+                        config->breakMinutes = value1;
+                    } else if (strcmp(key, "sessions") == 0) {
+                        config->sessions = value1;
+                    }
+                } else {
+                    printf("Error: Invalid duration or session values in config file. Using default values.\n");
+                }
+            }
         }
         fclose(file);
     } else {
         setDefaultConfig(config);
-        saveConfig(config, false);
-    } 
+    }
 }
 
 void setDefaultConfig(Config *config) {
@@ -56,14 +100,43 @@ void setDefaultConfig(Config *config) {
 }
 
 void saveConfig(Config *config, bool printMessage) {
+    char dirPath[256];
+    char *home = getenv("HOME");
+
+    if (home != NULL) {
+        snprintf(dirPath, sizeof(dirPath), "%s/.config/pomodoro", home);
+    } else {
+        // Home directory not found
+        fprintf(stderr, "Error: home directory not found.\n");
+        return;
+    }
+
+    struct stat st = {0};
+
+    if (stat(dirPath, &st) == -1) {
+        if (mkdir(dirPath, S_IRWXU) != 0) {  
+            perror("Error creating directory");
+            return;
+        }
+    }
+
+    expandUserHomeDirectory();
+
     FILE *file = fopen(CONFIG_FILE_PATH, "w");
+
+    if (!file) {
+        perror("Error opening the configuration file for writing");
+        return;
+    }
     if (file != NULL) {
-        fprintf(file, "%d %d %d %d %d %d %d %d %d", 
-                config->workRed, config->workGreen, config->workBlue, 
-                config->breakRed, config->breakGreen, config->breakBlue,
-                config->workMinutes, config->breakMinutes, config->sessions);
+        fprintf(file, "work=%d\n", config->workMinutes);
+        fprintf(file, "break=%d\n", config->breakMinutes);
+        fprintf(file, "sessions=%d\n", config->sessions);
+        fprintf(file, "workcolor=(%d,%d,%d)\n", config->workRed, config->workGreen, config->workBlue);
+        fprintf(file, "breakcolor=(%d,%d,%d)\n", config->breakRed, config->breakGreen, config->breakBlue);
         fclose(file);
-        if (printMessage){
+
+        if (printMessage) {
             printf("Pomodoro configuration saved\n");
         }
     } else {
@@ -75,6 +148,13 @@ void enableRawMode() {
     struct termios term;
     tcgetattr(STDIN_FILENO, &term);
     term.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &term);
+}
+
+void disableRawMode() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag |= (ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &term);
 }
 
@@ -103,7 +183,7 @@ void displayProgress(int totalSeconds, int elapsedSeconds, char* label, int sess
     remainingSeconds %= 60;
 
     printf("\033[H\033[J");
-    printf("Session %d\n", session);
+    printf("Session %d of %d\n", session, config->sessions);
 
     int remainingTime = totalSeconds / 60 - elapsedSeconds / 60;
     printf("%s for %d minute%s %d second%s.\n", 
@@ -136,9 +216,12 @@ void displayProgress(int totalSeconds, int elapsedSeconds, char* label, int sess
 }
 
 void sendNotification(char* message) {
-    char command[100];
-    sprintf(command, "osascript -e 'display notification \"%s\" with title \"Pomodoro\"'", message);
-    system(command);
+    char command[256];
+    snprintf(command, sizeof(command), "osascript -e 'display notification \"%s\" with title \"Pomodoro\"'", message);
+    int ret = system(command);
+    if(ret == -1) {
+        fprintf(stderr, "Failed to invoke system command.\n");
+    }
 }
 
 void updateConfigFromCommandLine(int argc, char *argv[], Config *tempConfig, bool *runTimer) {
@@ -146,12 +229,18 @@ void updateConfigFromCommandLine(int argc, char *argv[], Config *tempConfig, boo
         for (int i = 1; i < argc; ++i) {
             if (strstr(argv[i], "-")) {
                 int workPeriod, breakPeriod;
-                sscanf(argv[i], "%d-%d", &workPeriod, &breakPeriod);
+                if (2 != sscanf(argv[i], "%d-%d", &workPeriod, &breakPeriod)) {
+                    fprintf(stderr, "Error: Invalid work-break format. Using default values.\n");
+                    continue;
+                }
                 tempConfig->workMinutes = (workPeriod > 0) ? workPeriod : tempConfig->workMinutes;
                 tempConfig->breakMinutes = (breakPeriod > 0) ? breakPeriod : tempConfig->breakMinutes;
             } else if (strstr(argv[i], "n=")) {
                 int sessions;
-                sscanf(strstr(argv[i], "=") + 1, "%d", &sessions);
+                if (1 != sscanf(strstr(argv[i], "=") + 1, "%d", &sessions) || sessions <= 0) {
+                    fprintf(stderr, "Error: Invalid number of sessions value. Using default value.\n");
+                    continue;
+                }
                 tempConfig->sessions = (sessions > 0) ? sessions : tempConfig->sessions;
             } else if (strcmp(argv[i], "config") == 0) {
                 *runTimer = false;
@@ -159,32 +248,50 @@ void updateConfigFromCommandLine(int argc, char *argv[], Config *tempConfig, boo
                 while(i < argc) {
                     if (strstr(argv[i], "workcolor=")) {
                         int r, g, b;
-                        sscanf(strstr(argv[i], "=") + 1, "(%d,%d,%d)", &r, &g, &b);
-                        if (r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255) {
-                            tempConfig->workRed = r;
-                            tempConfig->workGreen = g;
-                            tempConfig->workBlue = b;
+                        if (3 != sscanf(strstr(argv[i], "=") + 1, "(%d,%d,%d)", &r, &g, &b) ||
+                            r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
+                            fprintf(stderr, "Error: Invalid workcolor format or values. Using default color.\n");
+                            i++;
+                            continue;
                         }
+                        tempConfig->workRed = r;
+                        tempConfig->workGreen = g;
+                        tempConfig->workBlue = b;
                     } else if (strstr(argv[i], "breakcolor=")) {
                         int r, g, b;
-                        sscanf(strstr(argv[i], "=") + 1, "(%d,%d,%d)", &r, &g, &b);
-                        if (r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255) {
-                            tempConfig->breakRed = r;
-                            tempConfig->breakGreen = g;
-                            tempConfig->breakBlue = b;
+                        if (3 != sscanf(strstr(argv[i], "=") + 1, "(%d,%d,%d)", &r, &g, &b) ||
+                            r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
+                            fprintf(stderr, "Error: Invalid breakcolor format or values. Using default color.\n");
+                            i++;
+                            continue;
                         }
+                        tempConfig->breakRed = r;
+                        tempConfig->breakGreen = g;
+                        tempConfig->breakBlue = b;
                     } else if (strstr(argv[i], "work=")) {
                         int work;
-                        sscanf(strstr(argv[i], "=") + 1, "%d", &work);
-                        tempConfig->workMinutes = (work > 0) ? work : tempConfig->workMinutes;
+                        if (1 != sscanf(strstr(argv[i], "=") + 1, "%d", &work) || work <= 0) {
+                            fprintf(stderr, "Error: Invalid work time. Using default value.\n");
+                            i++;
+                            continue;
+                        }
+                        tempConfig->workMinutes = work;
                     } else if (strstr(argv[i], "break=")) {
                         int breakPeriod;
-                        sscanf(strstr(argv[i], "=") + 1, "%d", &breakPeriod);
-                        tempConfig->breakMinutes = (breakPeriod > 0) ? breakPeriod : tempConfig->breakMinutes;
+                        if (1 != sscanf(strstr(argv[i], "=") + 1, "%d", &breakPeriod) || breakPeriod <= 0) {
+                            fprintf(stderr, "Error: Invalid break time. Using default value.\n");
+                            i++;
+                            continue;
+                        }
+                        tempConfig->breakMinutes = breakPeriod;
                     } else if (strstr(argv[i], "sessions=")) {
                         int sessions;
-                        sscanf(strstr(argv[i], "=") + 1, "%d", &sessions);
-                        tempConfig->sessions = (sessions > 0) ? sessions : tempConfig->sessions;
+                        if (1 != sscanf(strstr(argv[i], "=") + 1, "%d", &sessions) || sessions <= 0) {
+                            fprintf(stderr, "Error: Invalid sessions value. Using default value.\n");
+                            i++;
+                            continue;
+                        }
+                        tempConfig->sessions = sessions;
                     }
                     i++;
                 }
@@ -197,15 +304,35 @@ void updateConfigFromCommandLine(int argc, char *argv[], Config *tempConfig, boo
 }
 
 int main (int argc, char *argv[]) {
+    signal(SIGINT, handleSignal);
+    signal(SIGTERM, handleSignal);
+    signal(SIGHUP, handleSignal);
+    atexit(disableRawMode);
+
     enableRawMode();
     loadConfig(&config);
+    printf("Loaded config values:\n");
+    printf("Work Minutes: %d\n", config.workMinutes);
+    printf("Break Minutes: %d\n", config.breakMinutes);
+    printf("Sessions: %d\n", config.sessions);
+    printf("Work Color: (%d, %d, %d)\n", config.workRed, config.workGreen, config.workBlue);
+    printf("Break Color: (%d, %d, %d)\n", config.breakRed, config.breakGreen, config.breakBlue);
+
     
     Config tempConfig = config;
     bool runTimer = true;
 
     updateConfigFromCommandLine(argc, argv, &tempConfig, &runTimer);
+    printf("Loaded config values:\n");
+    printf("Work Minutes: %d\n", config.workMinutes);
+    printf("Break Minutes: %d\n", config.breakMinutes);
+    printf("Sessions: %d\n", config.sessions);
+    printf("Work Color: (%d, %d, %d)\n", config.workRed, config.workGreen, config.workBlue);
+    printf("Break Color: (%d, %d, %d)\n", config.breakRed, config.breakGreen, config.breakBlue);
+
     if (runTimer) {
         displayTimer(&tempConfig);
     }
+    disableRawMode();
     return 0;
 }
